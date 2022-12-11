07770fc5917bcb32799262ca9e125cc8319e2f2e
Doy Kim <kimdee.dev@gmail.com>
2022-12-11 14:43:37 +0900

    Initial Commit

---
+++ DoneDone-iOS/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DoneDone-iOS
+//
+//  Created by Doy Kim on 2022/12/11.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
